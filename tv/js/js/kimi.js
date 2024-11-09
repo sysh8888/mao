6df184708059c634b2298e017402f1ad10cf5fd3
sysh8888 <sysh8888@126.com>
2024-11-09 21:53:58 +0800

    Update kimi.js

--- tv/js/js/kimi.js
+++ tv/js/js/kimi.js
@@ -2,7 +2,7 @@ var rule = {
   title: 'KimiVod',
   host: 'https://kimivod.com',
   url: '/vod/show/id/fyclass/page/fypage.html',
-  searchUrl: '/vodsearch.html?wd=**',
+  searchUrl: 'https://cn.kimivod.com/search.php?page=fypage&searchword=**&searchtype=',
   searchable: 2,quickSearch: 0,filterable: 0,
   headers: {'User-Agent': 'MOBILE_UA'},
 class_name: '电影&剧集&综艺&动漫&短剧&国产剧&韩剧&美剧&日剧&台剧&港剧&海外剧&纪录片&泰剧&战争片&动画电影&悬疑片&奇幻片&爱情片&恐怖片&剧情片&动作片&科幻片&喜剧片&国产动漫&日本动漫&韩国动漫&欧美动漫&港台动漫&台港综艺&欧美综艺&韩日综艺&国产综艺',
